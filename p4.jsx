57e09d8cfc1595dcf0d5283452145dc9a6af0d43
Zachary <125631990+ZachPegram@users.noreply.github.com>
2023-09-29 13:07:58 -0400

    created the base files for problem 4

---
+++ p4.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Example from "./Components/example/Example";
+
+class p4 extends React.Component{
+
+//     const DynamicButton = ({ text, onClick, style }) => {
+//         return (
+//             <button onClick={onClick} style={style}>
+//                 {text}
+//             </button>
+//         );
+//     };
+//
+//
+}
+
+// export default p4;
