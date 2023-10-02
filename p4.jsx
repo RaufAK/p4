c725715e69fed1a73fa061eabd33918b419ef670
Zachary <125631990+ZachPegram@users.noreply.github.com>
2023-10-01 20:04:33 -0400

    Tried to add functionality to problem 4, code doesn't work. Not good with syntax on this.

--- p4.jsx
+++ p4.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import Example from "./Components/example/Example";
+import states from "./Components/states/States";
 
 class p4 extends React.Component{
+const mainButton = document.createElement('button');
+mainButton.textContent = 'Example View';
+document.body.appendChild(mainButton);
 
-//     const DynamicButton = ({ text, onClick, style }) => {
-//         return (
-//             <button onClick={onClick} style={style}>
-//                 {text}
-//             </button>
-//         );
-//     };
+mainButton.addEventListener('click', () => {
+    if(mainButton.textContent == "State View"){
+    window.location.src = /example/Example.jsx;
+    mainButton.textContent = "Example View";
+}else{
+    window.location.src = /states/States.jsx;
+    mainButton.textContent = "States View";
+
+}
+});
+
+
+
+
+
 //
 //
 }
